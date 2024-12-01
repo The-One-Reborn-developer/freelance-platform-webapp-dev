6ccc5fa75ec22873a548803d4c56915b361ef4c0
The One Reborn <mortonmarrion@gmail.com>
2024-12-01 19:10:53 +0300

    fix: typo in initData

--- app/public/register.js
+++ app/public/register.js
@@ -1,6 +1,6 @@
 if (window.Telegram && window.Telegram.WebApp) {
     console.log('Telegram WebApp object is accessible in register.js');
-    console.log('initData:', window.Telegram.WebApp.WebAppInitData);
+    console.log('initData:', window.Telegram.WebApp.initData);
 } else {
     console.error('Telegram WebApp is not accessible in register.js');
 }

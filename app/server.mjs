ab3be1c70ae9e4a13e3b2db639433b976bfaba83
The One Reborn <mortonmarrion@gmail.com>
2024-12-01 20:19:33 +0300

    debug: console log bot token

--- app/server.mjs
+++ app/server.mjs
@@ -51,6 +51,7 @@ app.post('/register', (req, res) => {
     };
 
     const botToken = process.env.TELEGRAM_BOT_TOKEN;
+    console.log(botToken);
     const secretKey = crypto.createHmac(
         'sha256',
         'WebAppData'

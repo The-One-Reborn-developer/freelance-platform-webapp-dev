3ae086f77e16f685ae87f69ae0d52d6fc7dc9dae
The One Reborn <mortonmarrion@gmail.com>
2024-12-02 15:36:04 +0300

    debug: remove console log from getUserData

--- app/public/main.js
+++ app/public/main.js
@@ -24,7 +24,6 @@ function getUserData(telegramID) {
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             return data;
         })
         .catch(error => {

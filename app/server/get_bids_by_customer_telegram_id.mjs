b60bc5687908fccbf65b4e525005a188a316554f
The One Reborn <mortonmarrion@gmail.com>
2024-12-04 15:07:04 +0300

    fix: SQL parameter customer_telegram_id

--- app/server/get_bids_by_customer_telegram_id.mjs
+++ app/server/get_bids_by_customer_telegram_id.mjs
@@ -1,9 +1,9 @@
-export function getBidsByCustomerTelegramID(db, telegram_id) {
+export function getBidsByCustomerTelegramID(db, customer_telegram_id) {
     try {
         const getBid = db.prepare(
-            'SELECT * FROM bids WHERE telegram_id = ? AND closed = FALSE'
+            'SELECT * FROM bids WHERE customer_telegram_id = ? AND closed = FALSE'
         );
-        const getBidResult = getBid.get(telegram_id);
+        const getBidResult = getBid.get(customer_telegram_id);
         return getBidResult;
     } catch (error) {
         console.error('Error in getUser:', error);

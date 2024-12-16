60bb93d15ec9af7b7d1a0939111b6ad94fa9dbb3
The One Reborn <mortonmarrion@gmail.com>
2024-12-17 01:30:41 +0300

    fix: define __dirname

--- app/server/server.mjs
+++ app/server/server.mjs
@@ -36,6 +36,9 @@ import { sendAttachment } from "./send_attachment.mjs";
 
 dotenv.config({ path: '/app/.env' });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 app.use(express.json());
 app.use(express.static('app/public'));

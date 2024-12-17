86aff17a657d65560da706aac6e9eca1fb6516d0
The One Reborn <mortonmarrion@gmail.com>
2024-12-17 22:52:47 +0300

    style: add styling to buttons

--- app/public/index.js
+++ app/public/index.js
@@ -3,4 +3,4 @@ window.Telegram.WebApp.disableVerticalSwipes()
 const servicesButton = document.getElementById('service-services-button');
 servicesButton.addEventListener('click', () => {
     window.location.href = 'services_register.html';
-})
+});

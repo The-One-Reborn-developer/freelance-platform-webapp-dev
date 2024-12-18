a4fa43d77418f97f5084b77315f1e28a4cf4fb88
The One Reborn <mortonmarrion@gmail.com>
2024-12-18 16:32:31 +0300

    fix: remove conditional logic in posting user

--- app/server/server.mjs
+++ app/server/server.mjs
@@ -97,10 +97,6 @@ app.post('/check-registration', (req, res) => {
 
 app.post('/registration-attempt', (req, res) => {
     try {
-        // Check which service the user is trying to register on
-        const service = req.body.service;
-        console.log(`service: ${service}`);
-
         // Check telegram data
         const telegramData = req.body.telegram_data;
         const telegramID = checkTelegramData(telegramData, res);
@@ -109,36 +105,34 @@ app.post('/registration-attempt', (req, res) => {
             console.error('Error in /registration-attempt: Telegram ID not found');
             res.status(500).json({ message: 'Произошла ошибка при регистрации пользователя.' });
         } else {
-            // Post the new user for services
-            if (service === 'services') {
-                const { role, name, rate, experience } = req.body;
-                postUser(
-                    db,
-                    res,
-                    telegramID,
-                    role,
-                    name,
-                    rate,
-                    experience,
-                    service
-                );
-            } else if (service === 'delivery') {
-                const { role, name, dateOfBirth, hasCar, carModel, carDimensionsWidth, carDimensionsLength, carDimensionsHeight } = req.body;
-                postUser(
-                    db,
-                    res,
-                    telegramID,
-                    role,
-                    name,
-                    dateOfBirth,
-                    hasCar,
-                    carModel,
-                    carDimensionsWidth,
-                    carDimensionsLength,
-                    carDimensionsHeight,
-                    service
-                );
-            };
+            const {
+                role,
+                name,
+                rate,
+                experience,
+                dateOfBirth,
+                hasCar,
+                carModel,
+                carDimensionsWidth,
+                carDimensionsLength,
+                carDimensionsHeight,
+                service
+            } = req.body;
+            
+            postUser(
+                db,
+                res,
+                telegramID,
+                role,
+                name,
+                dateOfBirth,
+                hasCar,
+                carModel,
+                carDimensionsWidth,
+                carDimensionsLength,
+                carDimensionsHeight,
+                service
+            );
         };
     } catch (error) {
         console.error('Error in /registration-attempt:', error);

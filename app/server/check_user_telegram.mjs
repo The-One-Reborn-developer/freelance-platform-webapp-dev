b21953a782e364186bbe52b84dca4f32daf45625
The One Reborn <mortonmarrion@gmail.com>
2024-12-18 22:48:23 +0300

    fix: change registered_in_deliveries to registered_in_delivery

--- app/server/check_user_telegram.mjs
+++ app/server/check_user_telegram.mjs
@@ -8,7 +8,7 @@ export function checkUserTelegram(db, telegramID, service) {
             return checkUserTelegramResult;   
         } else if (service === 'delivery') {
             const checkUserTelegram = db.prepare(
-                'SELECT COUNT(*) as count FROM users WHERE telegram_id = ? AND registered_in_deliveries = 1'
+                'SELECT COUNT(*) as count FROM users WHERE telegram_id = ? AND registered_in_delivery = 1'
             );
             const checkUserTelegramResult = checkUserTelegram.get(telegramID);
             return checkUserTelegramResult;

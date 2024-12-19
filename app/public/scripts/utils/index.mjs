b3d64effc710a190cad17e4629211219d7c99fb5
The One Reborn <mortonmarrion@gmail.com>
2024-12-19 17:10:10 +0300

    refactor: exports and imports in separate rows

--- app/public/scripts/utils/index.mjs
+++ app/public/scripts/utils/index.mjs
@@ -1,22 +1,17 @@
 import { fileToBase64 } from './file_to_base_64.mjs';
-export { fileToBase64 };
-
 import { scrollToBottom, scrollInputsIntoView } from './scrolls.mjs';
-export { scrollToBottom, scrollInputsIntoView };
-
 import { initializeWebSocket } from './websocket.mjs';
-export { initializeWebSocket };
-
 import { getQueryParameter } from "./parsing.mjs"
-export { getQueryParameter };
-
 import { getUserData } from "./requests.mjs"
-export { getUserData };
-
 import { showModal } from "./modals.mjs";
-export { showModal };
-
 import { fetchPerformers } from "../services/requests.mjs"
-export { fetchPerformers };
 import { fetchCustomers } from '../services/requests.mjs';
+
+export { fileToBase64 };
+export { scrollToBottom, scrollInputsIntoView };
+export { initializeWebSocket };
+export { getQueryParameter };
+export { getUserData };
+export { showModal };
+export { fetchPerformers };
 export { fetchCustomers };

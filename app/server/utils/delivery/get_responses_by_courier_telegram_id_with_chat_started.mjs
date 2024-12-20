f88c36ab019ef603bc66e7465748b27d27bf3965
The One Reborn <mortonmarrion@gmail.com>
2024-12-21 00:23:13 +0300

    fix: correct table name

--- app/server/utils/delivery/get_responses_by_courier_telegram_id_with_chat_started.mjs
+++ app/server/utils/delivery/get_responses_by_courier_telegram_id_with_chat_started.mjs
@@ -1,7 +1,7 @@
 export function getResponsesByCourierTelegramIDWithChatStarted(db, courierTelegramID) {
     try {
         const getResponsesByCourierTelegramIDWithChatStarted = db.prepare(
-            'SELECT * FROM delivery_responses WHERE courier_telegram_id = ? AND chat_started = TRUE'
+            'SELECT * FROM deliveries_responses WHERE courier_telegram_id = ? AND chat_started = TRUE'
         );
         const getResponsesByCourierTelegramIDWithChatStartedResult = getResponsesByCourierTelegramIDWithChatStarted.all(courierTelegramID);
         return getResponsesByCourierTelegramIDWithChatStartedResult;

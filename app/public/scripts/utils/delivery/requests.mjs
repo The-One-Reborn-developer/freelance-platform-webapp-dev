f2ad005c66752f71e67f756abbff03029ffb5c0b
The One Reborn <mortonmarrion@gmail.com>
2024-12-20 23:30:48 +0300

    fix: match mapping of returned data from server

--- app/public/scripts/utils/delivery/requests.mjs
+++ app/public/scripts/utils/delivery/requests.mjs
@@ -5,16 +5,16 @@ export async function fetchCouriers(validatedTelegramID) {
         console.log(`data: ${JSON.stringify(data)}`)
         if (data.success) {
             return data.responses.map((res) => ({
-                name: res.courier_name,
-                date_of_birth: res.courier_date_of_birth,
-                has_car: res.courier_has_car,
-                car_model: res.courier_car_model,
-                car_width: res.courier_car_width,
-                car_length: res.courier_car_length,
-                car_height: res.courier_car_height,
-                delivery_id: res.id,
-                telegram_id: res.courier_telegram_id,
-                registration_date: res.courier_registration_date
+                name: res.name,
+                date_of_birth: res.date_of_birth,
+                has_car: res.has_car,
+                car_model: res.car_model,
+                car_width: res.car_width,
+                car_length: res.car_length,
+                car_height: res.car_height,
+                delivery_id: res.delivery_id,
+                telegram_id: res.telegram_id,
+                registration_date: res.registration_date
             }));
         } else {
             return [];
@@ -28,7 +28,7 @@ export async function fetchCouriers(validatedTelegramID) {
 
 export async function fetchCustomers(validatedTelegramID) {
     try {
-        const response = await fetch(`/delivery/responded-customers?courier_telegram_id=${validatedTelegramID}`);
+        const response = await fetch(`/delivery/responded-customers?telegram_id=${validatedTelegramID}`);
         const data = await response.json();
 
         if (data.success && Array.isArray(data.deliveriesInfo)) {

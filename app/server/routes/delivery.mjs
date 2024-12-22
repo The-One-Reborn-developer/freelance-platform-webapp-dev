350037829021d51f7fb41798a627acba0421da0d
The One Reborn <mortonmarrion@gmail.com>
2024-12-22 12:41:41 +0300

    debug: console log incoming formData to server

--- app/server/routes/delivery.mjs
+++ app/server/routes/delivery.mjs
@@ -61,10 +61,7 @@ const deliveryRouter = express.Router();
 
 deliveryRouter.post('/upload-courier-photo', courier_photo.single('photo'), (req, res) => {
     try {
-        const courierTelegramID = req.body.courier_telegram_id;
-        const photo = req.file;
-
-        console.log(`Courier Telegram ID: ${courierTelegramID}`);
+        console.log(`Body: ${JSON.stringify(req.body)}`);
         res.status(200).json({ success: true, message: 'Фото успешно загружено.' });
     } catch (error) {
         console.error(`Error in /delivery/upload-courier-photo: ${error}`);

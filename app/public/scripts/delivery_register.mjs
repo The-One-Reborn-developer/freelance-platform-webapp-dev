8bc470b26b70828c14a36cec4cfc6daf9fbb242e
The One Reborn <mortonmarrion@gmail.com>
2024-12-22 11:54:35 +0300

    debug: console log incoming data from /common/check-registration

--- app/public/scripts/delivery_register.mjs
+++ app/public/scripts/delivery_register.mjs
@@ -47,7 +47,6 @@ window.onload = function () {
     initializePage();
     // Check if the user is already registered
     const telegramData = window.Telegram.WebApp.initData;
-    console.log(telegramData);
     const userTelegramID = checkIfUserIsRegistered(telegramData);
     console.log(userTelegramID);
     // Ensure that the keyboard is closed when the user touches the screen outside of input elements
@@ -73,6 +72,7 @@ function checkIfUserIsRegistered(telegramData) {
             console.log(data);
             window.location.href = `delivery_main.html?telegram_id=${encodeURIComponent(data.telegram_id)}`;  // Redirect if the user is registered
         } else {
+            console.log(data);
             return data.telegram_id;
         };
     })

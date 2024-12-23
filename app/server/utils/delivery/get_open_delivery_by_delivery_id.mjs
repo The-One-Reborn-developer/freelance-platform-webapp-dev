7e87cc4ebef3fd6e46814d3445ae1ccc8666429a
The One Reborn <mortonmarrion@gmail.com>
2024-12-23 14:17:08 +0300

    fix: typo in table name

--- app/server/utils/delivery/get_open_delivery_by_delivery_id.mjs
+++ app/server/utils/delivery/get_open_delivery_by_delivery_id.mjs
@@ -1,12 +1,12 @@
 export function getOpenDeliveryByDeliveryID(db, deliveryID) {
     try {
         const getDelivery = db.prepare(
-            'SELECT * FROM Deliverys WHERE id = ? AND closed = FALSE'
+            'SELECT * FROM deliveries WHERE id = ? AND closed = FALSE'
         );
         const getDeliveryResult = getDelivery.get(deliveryID);
         return getDeliveryResult;
     } catch (error) {
-        console.error('Error in getDeliveryByDeliveryID:', error);
+        console.error('Error in getOpenDeliveryByDeliveryID:', error);
         return null;
     };
 };

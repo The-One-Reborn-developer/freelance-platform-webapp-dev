07d2a82a173e04555b51efcf23b3502b61ee748b
The One Reborn <mortonmarrion@gmail.com>
2024-12-27 11:00:19 +0300

    fix: put f flags after command

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker-compose down -f docker-compose-dev.yml -v
-sudo docker-compose build -f docker-compose-dev.yml --no-cache
-sudo docker-compose up -f docker-compose-dev.yml
+sudo docker-compose -f down docker-compose-dev.yml -v
+sudo docker-compose -f build docker-compose-dev.yml --no-cache
+sudo docker-compose -f up docker-compose-dev.yml

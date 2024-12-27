661a180d87f180d9d64872d40208219e217a7429
The One Reborn <mortonmarrion@gmail.com>
2024-12-27 10:55:45 +0300

    fix: remove git pull from setup scripts

--- dev.sh
+++ dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull
 sudo docker-compose down -f docker-compose-dev.yml -v
 sudo docker-compose build -f docker-compose-dev.yml --no-cache
 sudo docker-compose up -f docker-compose-dev.yml

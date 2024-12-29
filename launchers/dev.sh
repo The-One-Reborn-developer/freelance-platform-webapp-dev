294af7c2875e274c49da83fec07b97424d1ad8fe
The One Reborn <mortonmarrion@gmail.com>
2024-12-29 17:20:44 +0300

    refactor: move dockerfiles to appropriate directory

--- launchers/dev.sh
+++ launchers/dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname "$0")/.."
-sudo docker-compose -f docker-compose-dev.yml down -v
-sudo docker-compose -f docker-compose-dev.yml build --no-cache
-sudo docker-compose -f docker-compose-dev.yml up
+sudo docker-compose -f dockerfiles/docker-compose-dev.yml down -v
+sudo docker-compose -f dockerfiles/docker-compose-dev.yml build --no-cache
+sudo docker-compose -f dockerfiles/docker-compose-dev.yml up

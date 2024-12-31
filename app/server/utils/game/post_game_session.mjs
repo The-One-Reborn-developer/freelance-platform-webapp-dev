8e88f2e67610fe5377d5bfd8635f81caa89fdb9a
The One Reborn <mortonmarrion@gmail.com>
2024-12-31 21:43:02 +0300

    refactor: error handling and returns in postGameSession

--- app/server/utils/game/post_game_session.mjs
+++ app/server/utils/game/post_game_session.mjs
@@ -1,25 +1,34 @@
 export function postGameSession(db, sessionID) {
-    try {
-        if (!sessionID) {
-            console.error('Session ID not provided');
-            return false;
-        };
+    if (!sessionID) {
+        console.error('Session ID not provided');
+        return {
+            success: false,
+            status: 400,
+            message: 'Session ID not provided'
+        }
+    };
 
-        const existingGameSession = db.prepare(
-            'SELECT * FROM game_sessions WHERE session_id = ?'
-        ).get(sessionID);
+    const existingGameSession = db.prepare(
+        'SELECT * FROM game_sessions WHERE session_id = ?'
+    ).get(sessionID);
 
-        if (existingGameSession) {
-            return 'Game session already exists';
-        };
+    if (existingGameSession) {
+        return {
+            success: false,
+            status: 409,
+            message: `Сессия с ID ${sessionID} уже существует`
+        }
+    };
 
-        const postGameSession = db.prepare(
-            'INSERT INTO game_sessions (session_id) VALUES (?)'
-        );
-        const postGameSessionResult = postGameSession.run(sessionID);
-        
-        return postGameSessionResult;
-    } catch (error) {
-        console.error('Error in postGameSession:', error);
+    const postGameSession = db.prepare(
+        'INSERT INTO game_sessions (session_id) VALUES (?)'
+    );
+    const postGameSessionResult = postGameSession.run(sessionID);
+    
+    const newGameSessionID = postGameSessionResult.lastInsertRowid;
+    return {
+        success: true,
+        status: 201,
+        message: `Сессия с ID ${newGameSessionID} успешно создана`
     };
 };
